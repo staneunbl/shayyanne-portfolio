d92f7f583cbf74beb34cf24f383d691ca5f0c01c
Shayyanne Marasigan <shayyannedominiq.marasigan@letran.edu.ph>
2024-12-07 21:05:08 +0800

    variable changed names again to correct

--- src/components/MiddleBlock/index.tsx
+++ src/components/MiddleBlock/index.tsx
@@ -31,31 +31,31 @@ const MiddleBlock = ({ title, content, button, t }: MiddleBlockProps) => {
                 {/* First row */}
                 <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
                   <div>
-                    <img src="/img/svg/HTML.svg" alt="HTML" width="65" height="65" />
+                    <img src="/img/svg/html.svg" alt="HTML" width="65" height="65" />
                   </div>
                   <div>
-                    <img src="/img/svg/CSS.svg" alt="CSS" width="65" height="65" />
+                    <img src="/img/svg/css.svg" alt="CSS" width="65" height="65" />
                   </div>
                   <div>
-                    <img src="/img/svg/JavaScript.svg" alt="JavaScript" width="65" height="65" />
+                    <img src="/img/svg/javascript.svg" alt="JavaScript" width="65" height="65" />
                   </div>
                   <div>
-                    <img src="/img/svg/React.svg" alt="React" width="65" height="65" />
+                    <img src="/img/svg/react.svg" alt="React" width="65" height="65" />
                   </div>
                   <div>
-                    <img src="/img/svg/Angular.svg" alt="Angular" width="65" height="65" />
+                    <img src="/img/svg/angular.svg" alt="Angular" width="65" height="65" />
                   </div>
                   <div>
-                    <img src="/img/svg/PHP.svg" alt="PHP" width="65" height="65" />
+                    <img src="/img/svg/php.svg" alt="PHP" width="65" height="65" />
                   </div>
                 </div>
                 {/* Second row */}
                 <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
                   <div>
-                    <img src="/img/svg/Figma.svg" alt="Figma" width="65" height="65" />
+                    <img src="/img/svg/figma.svg" alt="Figma" width="65" height="65" />
                   </div>
                   <div>
-                    <img src="/img/svg/Bootstrap.svg" alt="Glide Apps" width="65" height="65" />
+                    <img src="/img/svg/bootstrap.svg" alt="Glide Apps" width="65" height="65" />
                   </div>
                   <div>
                     <img src="/img/svg/AdobePremierePro.svg" alt="Adobe Premiere Pro" width="65" height="65" />
@@ -64,7 +64,7 @@ const MiddleBlock = ({ title, content, button, t }: MiddleBlockProps) => {
                     <img src="/img/svg/AdobeIllustrator.svg" alt="Adobe Illustrator" width="65" height="65" />
                   </div>
                   <div>
-                    <img src="/img/svg/Canva.svg" alt="Canva" width="65" height="65" />
+                    <img src="/img/svg/canva.svg" alt="Canva" width="65" height="65" />
                   </div>
                 </div>
               </div>
